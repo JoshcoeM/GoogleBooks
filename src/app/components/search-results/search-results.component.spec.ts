4900ccdb04da3944968aa0cae019317d199a93b2
Josh Moscoe <jmoscoe@manifestcorp.com>
2021-12-13 13:42:38 -0500

    GoogleBooks

--- src/app/components/search-results/search-results.component.spec.ts
+++ src/app/components/search-results/search-results.component.spec.ts
@@ -1,12 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/providers/book.service';
+import { BookComponent } from '../book/book.component';
 
 import { SearchResultsComponent } from './search-results.component';
 
 class MockActivatedRoute{
   queryParams: Observable<Params> = new Observable<Params>();
+}
+
+class MockBookService {
+  getSearchResults(term:String): Observable<Book> {
+    return of();
+  }
 }
 
 describe('SearchResultsComponent', () => {
@@ -15,13 +24,10 @@ describe('SearchResultsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchResultsComponent ],
+      declarations: [ SearchResultsComponent, BookComponent ],
       providers: [
-        {
-          provide: ActivatedRoute,
-          useClass: MockActivatedRoute
-        }
-      ]
+        { provide: ActivatedRoute, useClass: MockActivatedRoute },
+        { provide : BookService, useClass: MockBookService}]
     })
     .compileComponents();
   });
@@ -37,14 +43,30 @@ describe('SearchResultsComponent', () => {
   });
 
   describe ('class', () =>{
-    it('subscribes to the queryParams', () => {
-      const route: ActivatedRoute = TestBed.inject(ActivatedRoute);
-      spyOn(route.queryParams, 'subscribe').and.callFake((callback : any) => {
-        return callback( {term: '123'});
+    describe('ngOnInit', () => {
+      it('subscribes to the queryParams', () => {
+        const route: ActivatedRoute = TestBed.inject(ActivatedRoute);
+        spyOn(route.queryParams, 'subscribe').and.callFake((callback : any) => {
+          return callback( {term: '123'});
+        });
+        component.ngOnInit();
+        expect(route.queryParams.subscribe).toHaveBeenCalled();
+        expect(component.term).toBe('123'); 
       });
-      component.ngOnInit();
-      expect(route.queryParams.subscribe).toHaveBeenCalled();
-      expect(component.term).toBe('123'); 
+
+      it('gets the results for the term from the book service and stores them',() => {
+        const route: ActivatedRoute = TestBed.inject(ActivatedRoute);
+        spyOn(route.queryParams, 'subscribe').and.callFake((callback : any) => {
+          return callback({term : '123'});          
+        });
+        let book: Book = new Book();
+        book.title = 'test title';
+        spyOn(component.bookService, 'getSearchResults').and.returnValue(of(book));
+        component.ngOnInit();
+        expect(component.bookService.getSearchResults).toHaveBeenCalledWith('123');
+        expect(component.results).toBeDefined();
+        expect(component.results).toEqual([book]);
+      });
     });
   });
 
--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Book } from 'src/app/models/book';
+import { BookService } from 'src/app/providers/book.service';
 
 @Component({
   selector: 'gb-search-results',
@@ -8,13 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchResultsComponent implements OnInit {
 
+  results : Book[] = [];
   term : String ="";
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(public route: ActivatedRoute, public bookService : BookService) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       this.term = params['term'];
+      this.bookService.getSearchResults(this.term).subscribe((book: Book) => {
+        this.results.push(book);
+      });
     });
   }
 
